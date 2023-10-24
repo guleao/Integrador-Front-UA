6fbfb7759a77f0146a0c674827c94256d97e61ab
guleao <guztavusz@gmail.com>
2023-10-24 12:06:09 -0300

    Iniciando Telas P.I

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './sistema/login/login.component';
+import { RecuperarComponent } from './sistema/recuperar/recuperar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "recuperar", component: RecuperarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './sistema/login/login.component';
+import { IndexComponent } from './layout/index/index.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { RecuperarComponent } from './sistema/recuperar/recuperar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent,
+    RecuperarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
